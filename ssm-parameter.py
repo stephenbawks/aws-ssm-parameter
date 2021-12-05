1ac2890ca3c0056929936683a5ba36dbff530657
gigadafud <stephen@bawks.com>
2021-12-05 16:22:13 -0500

    couple updates

--- ssm-parameter.py
+++ ssm-parameter.py
@@ -37,24 +37,30 @@ parser.add_argument(
     help="AWS SSM Parameter Value"
 )
 
-# parser.add_argument(
-#     "--list_of_values",
-#     metavar='list_of_values',
-#     type=list,
-#     help="AWS SSM List of Parameter Values"
-# )
+parser.add_argument(
+    "--description",
+    metavar='description',
+    type=str,
+    help="AWS SSM Parameter Description"
+)
 
 args = parser.parse_args()
 
 
 def check_exists_ssm_parameter(parameter_name: str) -> bool:
+    """
+    Check to see if a AWS SSM Parameter exists
 
+    Returns:
+        (bool): True or False 
+    """
     ssm = boto3.client('ssm')
     # Check if the ssm parameter exists
     try:
         # Get the value of the ssm parameter
         ssm.get_parameter(Name=parameter_name, WithDecryption=True)
         # If the parameter exists, return the value
+        print("Parameter Exists, checking to see if Value is up to date.")
         return True
     except ClientError as e:
         # If the parameter does not exist, return None
@@ -63,9 +69,13 @@ def check_exists_ssm_parameter(parameter_name: str) -> bool:
         else:
             raise
 
-def check_value_ssm_parameter(parameter_name: str, parameter_value: str) -> bool:
+def check_value_ssm_parameter(parameter_name: str, parameter_value: str, parameter_description: str="") -> bool:
     """
+    Check to see if the value of a AWS SSM Parameter is up to date
+    If it is not, it will update it
 
+    Returns:
+        (bool): True or False 
     """
     ssm = boto3.client('ssm')
 
@@ -84,19 +94,47 @@ def check_value_ssm_parameter(parameter_name: str, parameter_value: str) -> bool
             update_response = ssm.put_parameter(
                 Name=parameter_name,
                 Value=parameter_value,
+                Description=parameter_description,
                 Type='SecureString',
                 Overwrite=True,
                 Tier='Intelligent-Tiering',
                 DataType='text'
             )
             print("Parameter has been updated.")
+            return True
+    except:
+        value = None
+
+def create_ssm_parameter(parameter_name: str, parameter_value: str, parameter_description: str="") -> bool:
+    """
+    Create a AWS SSM Parameter
+
+    Returns:
+        (bool): True or False 
+    """
+    ssm = boto3.client('ssm')
+
+    print("Parameter does not exist, creating now.....")
+    try:
+        response = ssm.put_parameter(
+            Name=parameter_name,
+            Value=parameter_value,
+            Description=parameter_description,
+            Type='SecureString',
+            Overwrite=True,
+            Tier='Intelligent-Tiering',
+            DataType='text'
+        )
+        print("Parameter has been created.")
+        return True
     except:
         value = None
 
 
 check_exists = check_exists_ssm_parameter(parameter_name=args.name)
 
-if check_exists:
-    print("Parameter Exists, checking to see if Value is up to date.")
-    check_value_ssm_parameter(parameter_name=args.name,
-                              parameter_value=args.value)
+if check_exists == True:
+    check_value_ssm_parameter(parameter_name=args.name, parameter_value=args.value, parameter_description=args.description)
+elif check_exists == False:
+    create_ssm_parameter(parameter_name=args.name, parameter_value=args.value, parameter_description=args.description)
+
